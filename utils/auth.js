09a8e0df835f506a37f912f1f980ab1ec2aadfcd
Drew-Andersen <andrewtandersen@gmail.com>
2024-08-08 18:56:47 -0400

    Fixed the utils/auth.js

--- utils/auth.js
+++ utils/auth.js
@@ -1,9 +1,9 @@
 // User Auth
 const withAuth = (req, res, next) => {
-    console.log(req.session.loggedIn);
+    console.log(req.session.logged_in);
 
     // If the user is NOT logged in, redirct them to the login page
-    if (!req.session/loggedIn) {
+    if (!req.session.logged_in) {
         res.redirect('/login')
     } else {
         next();
